8c40310ad3d5685b10b77a5c317dfd7db0933fb6
Dicky Agusputra <loscarbulos@gmail.com>
2024-01-04 10:15:57 +0700

    Dashboard Widgets

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Announcement;
+use App\Models\FamilyCard;
 use App\Models\News;
+use App\Models\Resident;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -19,12 +21,22 @@ class DashboardController extends Controller
         $users = User::where('level', 'user')->count();
         $news = News::count();
         $announcements = Announcement::count();
+        $familyCards = FamilyCard::count();
+        $residents = Resident::count();
+
+        // Data for admin hero section
+        $newsPosted = News::where('user_id', auth()->user()->id)->count();
+        $announcementPosted = Announcement::where('user_id', auth()->user()->id)->count();
 
         return view('admin-panel.pages.dashboard', compact(
             'admins',
             'users',
             'news',
-            'announcements'
+            'announcements',
+            'familyCards',
+            'residents',
+            'newsPosted',
+            'announcementPosted'
         ));
     }
 }
--- resources/views/admin-panel/pages/dashboard.blade.php
+++ resources/views/admin-panel/pages/dashboard.blade.php
@@ -10,8 +10,8 @@
                 <div class="card border-0 zoom-in bg-light-primary shadow-none">
                     <div class="card-body">
                         <div class="text-center">
-                            <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-user-male.svg"
-                                width="50" height="50" class="mb-1" alt="" />
+                            <img src="{{ asset('panel-assets/dist/images/iconscout/admin.png') }}" class="mb-1"
+                                alt="" />
                             <p class="fw-semibold fs-3 text-primary mb-1"> Total Admin </p>
                             <h5 class="fw-semibold text-primary mb-0">{{ $admins }}</h5>
                         </div>
@@ -19,25 +19,13 @@
                 </div>
             </div>
             <div class="item">
-                <div class="card border-0 zoom-in bg-light-warning shadow-none">
+                <div class="card border-0 zoom-in bg-light-secondary shadow-none">
                     <div class="card-body">
                         <div class="text-center">
-                            <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-briefcase.svg"
-                                width="50" height="50" class="mb-1" alt="" />
+                            <img src="{{ asset('panel-assets/dist/images/iconscout/teamwork.png') }}" class="mb-1"
+                                alt="" />
                             <p class="fw-semibold fs-3 text-warning mb-1">Total User</p>
                             <h5 class="fw-semibold text-warning mb-0">{{ $users }}</h5>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="item">
-                <div class="card border-0 zoom-in bg-light-info shadow-none">
-                    <div class="card-body">
-                        <div class="text-center">
-                            <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-mailbox.svg"
-                                width="50" height="50" class="mb-1" alt="" />
-                            <p class="fw-semibold fs-3 text-info mb-1">Berita </p>
-                            <h5 class="fw-semibold text-info mb-0">{{ $news }}</h5>
                         </div>
                     </div>
                 </div>
@@ -46,10 +34,46 @@
                 <div class="card border-0 zoom-in bg-light-success shadow-none">
                     <div class="card-body">
                         <div class="text-center">
-                            <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-mailbox.svg"
-                                width="50" height="50" class="mb-1" alt="" />
+                            <img src="{{ asset('panel-assets/dist/images/iconscout/news.png') }}" class="mb-1"
+                                alt="" />
+                            <p class="fw-semibold fs-3 text-info mb-1">Berita </p>
+                            <h5 class="fw-semibold text-info mb-0">{{ $news }}</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 zoom-in bg-light-danger shadow-none">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <img src="{{ asset('panel-assets/dist/images/iconscout/megaphone.png') }}" class="mb-1"
+                                alt="" />
                             <p class="fw-semibold fs-3 text-info mb-1">Pengumuman </p>
                             <h5 class="fw-semibold text-info mb-0">{{ $announcements }}</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 zoom-in bg-light-warning shadow-none">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <img src="{{ asset('panel-assets/dist/images/iconscout/card.png') }}" class="mb-1"
+                                alt="" />
+                            <p class="fw-semibold fs-3 text-info mb-1">Total KK </p>
+                            <h5 class="fw-semibold text-info mb-0">{{ $familyCards }}</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 zoom-in bg-light-info shadow-none">
+                    <div class="card-body">
+                        <div class="text-center">
+                            <img src="{{ asset('panel-assets/dist/images/iconscout/family.png') }}" class="mb-1"
+                                alt="" />
+                            <p class="fw-semibold fs-3 text-info mb-1">Total Penduduk </p>
+                            <h5 class="fw-semibold text-info mb-0">{{ $residents }}</h5>
                         </div>
                     </div>
                 </div>
@@ -61,6 +85,43 @@
                 <canvas id="myChart"></canvas>
             </div>
         </div> --}}
+        <div class="row">
+            <div class="col-12">
+                <div class="card w-100 bg-info-subtle overflow-hidden shadow-none">
+                    <div class="card-body position-relative">
+                        <div class="row">
+                            <div class="col-sm-7">
+                                <div class="d-flex align-items-center mb-7">
+                                    <div class="rounded-circle overflow-hidden me-6">
+                                        <img src="{{ auth()->user()->avatar }}" alt="" width="40"
+                                            height="40">
+                                    </div>
+                                    <h5 class="fw-semibold mb-0 fs-5">Selamat Datang {{ auth()->user()->name }}!</h5>
+                                </div>
+                                <div class="d-flex align-items-center">
+                                    <div class="border-end pe-4 border-muted border-opacity-10">
+                                        <h3 class="mb-1 fw-semibold fs-8 d-flex align-content-center">{{ $newsPosted }}<i
+                                                class="ti ti-arrow-up-right fs-5 lh-base text-success"></i></h3>
+                                        <p class="mb-0 text-dark">Berita yang anda posting</p>
+                                    </div>
+                                    <div class="ps-4">
+                                        <h3 class="mb-1 fw-semibold fs-8 d-flex align-content-center">{{ $announcementPosted }}<i
+                                                class="ti ti-arrow-up-right fs-5 lh-base text-success"></i></h3>
+                                        <p class="mb-0 text-dark">Pengumuman yang anda posting</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-5">
+                                <div class="welcome-bg-img mb-n7 text-end">
+                                    <img src="{{ asset('panel-assets/dist/images/backgrounds/welcome-bg.svg') }}"
+                                        alt="" class="img-fluid">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
