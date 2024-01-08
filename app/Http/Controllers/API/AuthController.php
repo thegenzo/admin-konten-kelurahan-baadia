cbb6c9749a217d925054901376db2b572ff6610a
Dicky Agusputra <loscarbulos@gmail.com>
2024-01-08 13:01:52 +0700

    API for authentication

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $rules = [
+            'email'               => 'required|email',
+            'password'            => 'required'
+        ];
+
+        $messages = [
+            'email.required'      => 'Email wajib diisi',
+            'email.email'         => 'Email harus berformat email',
+            'password.required'   => 'Password wajib diisi',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Please fill the required data',
+                'data' => $validator->errors()
+            ], 400);
+        }
+
+        if(! Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if($user && Hash::check($request->password, $user->password)) {
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Login success',
+                'access_token' => $token,
+                'token_type' => 'Bearer'
+            ]);    
+        } else {
+            return response()->json([
+                'message' => 'Login failed',
+            ]); 
+        }
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Logout success'
+        ]);
+    }
+
+    public function checkIfIdNumberExist(Request $request)
+    {
+        $rules = [
+            'id_number'               => 'required|exists:residents,id_number',
+        ];
+
+        $messages = [
+            'id_number.required'      => 'NIK tidak boleh kosong',
+            'id_number.exists'        => 'NIK tidak terdaftar',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Please fill the required data',
+                'data' => $validator->errors()
+            ], 400);
+        }
+
+        return response()->json([
+            'messages' => 'NIK terdaftar!'
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $rules = [
+            'name'      => 'required',
+            // 'avatar'    => 'image|mimes:jpeg,png,jpg',
+            'email'     => 'required|email|unique:users',
+            'password'  => 'required|min:8|confirmed',
+        ];
+
+        $messages = [
+            'name.required'         => 'Nama user wajib diisi',
+            // 'avatar.image'          => 'Avatar harus berupa gambar',
+            // 'avatar.mimes'          => 'Avatar harus berformat gambar (jpeg, png atau jpg)',
+            'email.required'        => 'Email user wajib diisi',
+            'email.email'           => 'Email user harus berformat email',
+            'email.unique'          => 'Email user sudah terpakai',
+            'password.required'     => 'Password wajib diisi',
+            'password.min'          => 'Password harus berupa 8 karakter',
+            'password.confirmed'    => 'Password harus sama dengan konfirmasi password',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Please fill the required data',
+                'data' => $validator->errors()
+            ], 400);
+        }
+
+        $data = $request->all();
+        $data['level'] = 'user';
+        $data['avatar'] = $request->level == 'admin' ? 'https://static-00.iconduck.com/assets.00/user-icon-2048x2048-ihoxz4vq.png' : 'https://cdn-icons-png.flaticon.com/512/945/945120.png';
+
+        $user = User::create($data);
+
+        // generate token
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'data'  => $user,
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
+}
---
+++ app/Http/Controllers/API/NewsController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\News;
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $news = News::with('user')->paginate(10);
+
+        return response()->json($news);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $news = News::find($id);
+
+        return response()->json($news);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
