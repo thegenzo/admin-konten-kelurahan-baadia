1b6515470de8b98fa2fe21b26f9d7ff9b5d2ff36
Dicky Agusputra <loscarbulos@gmail.com>
2024-01-12 15:12:24 +0700

    Update News.php

--- app/Models/News.php
+++ app/Models/News.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class News extends Model
 {
@@ -20,5 +21,10 @@ class News extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function getContentAttribute()
+    {
+        return htmlspecialchars($this->attributes['content']);
     }
 }
