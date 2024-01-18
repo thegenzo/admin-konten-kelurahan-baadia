f9d3235afd305c1a721634f86f1bb4ae463be8e0
Dicky Agusputra <loscarbulos@gmail.com>
2024-01-18 20:52:59 +0700

    minor fix

--- resources/views/admin-panel/pages/announcement/edit.blade.php
+++ resources/views/admin-panel/pages/announcement/edit.blade.php
@@ -70,7 +70,7 @@
                             </div>
                             <div class="form-group mb-3">
 								<label for="content">Konten Pengumuman <span class="text-danger">*</span></label>
-								<textarea name="content" id="" cols="30" rows="50" class="form-control">{!! $announcement->content !!}</textarea>
+								<textarea name="content" id="" cols="30" rows="10" class="form-control">{!! $announcement->content !!}</textarea>
                             </div>
                             <div class="form-group mb-3">
                                 <label for="status">Status <span class="text-danger">*</span></label>
